577a5282b08c6f68d6427a52a12501406d5f9f6b
Dariusz Urbanczyk <dare2u@gmail.com>
2019-07-31 15:29:39 +0100

    calc buttons completed using computed properties

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,23 +14,31 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
-    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return  number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
          isFinishedTypingNumber = true
-        guard let number = Double(displayLabel.text!) else {
-        fatalError("Cannot convert display label text to a Double.")
-        }
+   
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue = displayValue * -1
             
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             } else if calcMethod == "%" {
-            displayLabel.text =  String(number / 100)
+            displayValue = displayValue * 0.01
                     }
                 }
             }
@@ -47,13 +55,11 @@ class ViewController: UIViewController {
             if isFinishedTypingNumber {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
-                // prevents adding more than one "." in a line
+                // prevents adding more than one "." in a line ie. 8.2.1 numbers
             } else {
                 if numValue == "." {
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert display label text to a Double")
-                    }
-                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+         
+                    let isInt = floor(displayValue) == displayValue
                     if !isInt {
                         return
                     }
